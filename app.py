9b251b616f0f9f06abc6513794e1f3dcb1ebcbaa
Chibuikem <chibuikemalalieteh@gmail.com>
2022-05-19 20:34:38 +0100

    db ping

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
-from project import create_app
+from project import create_app, db
 app = create_app()
 
-
+@app.before_request
+def ping_db():
+    '''
+    database server keeps closing connection unexpectedly,
+    so pind database and pass exception before main request
+    to it
+    '''
+    try:
+        db.engine.execute('SELECT 1')
+    except Exception as e:
+        print('i just executed')
 
 if __name__ == '__main__':
     
